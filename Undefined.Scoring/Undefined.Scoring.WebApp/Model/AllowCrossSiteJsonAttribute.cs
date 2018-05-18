33ab795615d0422f2dd37e7a8e35fbb643ee92fd
arxen42 <arxen42@tutanota.com>
2018-05-19 00:57:33 +0300

    Initial project

---
+++ Undefined.Scoring/Undefined.Scoring.WebApp/Model/AllowCrossSiteJsonAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Undefined.Scoring.WebApp.Model
+{
+	public class AllowCrossSiteJsonAttribute: ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			context.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+			base.OnActionExecuting(context);
+		}
+	}
+}
---
+++ Undefined.Scoring/Undefined.Scoring.WebApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Undefined.Scoring.WebApp
+{
+	public static class Program
+	{
+		public static void Main(String[] args)
+		{
+			BuildWebHost(args).Run();
+		}
+
+		private static IWebHost BuildWebHost(String[] args)
+		{
+			return WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseUrls("http://localhost")
+				.Build();
+		}
+	}
+}
